b9cc6e834faa13ae60fc1b002b01f5c00e8fb3d8
Justin Melger <jmelger@puurdata.nl>
2025-11-10 21:45:53 +0100

    feat: add dumm database client

--- app/api/ingest.py
+++ app/api/ingest.py
@@ -5,5 +5,4 @@ router = APIRouter()
 
 @router.post("/exchange_rates/ingest")
 async def ingest_exchange_rates():
-
     return {"status": "success"}
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api import ingest
 
+
 app = FastAPI()
 
 
