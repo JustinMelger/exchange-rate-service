4a1062347ddccda854ee182b89e724f615247d3d
Justin Melger <jmelger@puurdata.nl>
2025-11-10 22:00:23 +0100

    feat: dummy flow

--- app/api/ingest.py
+++ app/api/ingest.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
+from app.external_clients.open_exchange import OpenExchangeClient
+from app.database.bigquery import BigQueryClient
+from app.services.ingress_service import ExchangeRateIngressService
 
 router = APIRouter()
 
 
 @router.post("/exchange_rates/ingest")
 async def ingest_exchange_rates():
-    return {"status": "success"}
+    open_exchange_client = OpenExchangeClient()
+    database_client = BigQueryClient()
+    exchange_rates = ExchangeRateIngressService(exchange_client=open_exchange_client, bigquery_client=database_client)
+    return exchange_rates.ingest_historical_rates()
